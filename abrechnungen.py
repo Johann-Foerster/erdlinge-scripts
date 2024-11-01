519a39f6554540693f56769ad33f24f2573c9bfb
Johann Förster <johann.foerster@qualityminds.de>
2024-11-01 18:25:53 +0100

    more parsing

--- abrechnungen.py
+++ abrechnungen.py
@@ -1,12 +1,12 @@
 from tika import parser
 from dataclasses import dataclass, field
-import glob, csv
+import glob, csv, sys
 
 pdfs = glob.glob("abrechnungen/2024/*.pdf")
 data = []
 
 def get_pages(filename):
-    raw_xml = parser.from_file(pdf, xmlContent=True)
+    raw_xml = parser.from_file(filename, xmlContent=True)
     body = raw_xml['content'].split('<body>')[1].split('</body>')[0]
     body_without_tag = body.replace("<p>", "").replace("</p>", "").replace("<div>", "").replace("</div>","").replace("<p />","")
     text_pages = body_without_tag.split("""<div class="page">""")[1:]
@@ -16,32 +16,51 @@ def get_pages(filename):
         exit(1)
     return text_pages
 
+
+def parse_float(float_str_eu: str):
+    return float(float_str_eu.replace(".", "").replace(",","."))
+
 @dataclass
 class Page:
     month_year: str = field()
     name: str = field()
+    arbeitsmarktzulage: float = field()
+    muenchenzulage: float = field()
     is_rueckrechnung: bool = field()
-    gesamtbrutto: str = field()
 
     def __init__(self, page: str):
         # Initialize fields with default values
-        lines = page.split('\n')
+        self._lines = page.split('\n')
 
 
-        self.month_year = [line for line in lines if "Gehaltsabrechnung" in line][0].split(" ")[-1]
+        self.month_year = self.line_with("Gehaltsabrechnung").split(" ")[-1]
 
-        self.name = [line for line in lines if " Abteilung" in line][0].split(" Abteilung")[0]
+        self.name = self.line_with(" Abteilung").split(" Abteilung")[0]
 
         self.is_rueckrechnung = "Rückrechnung" in page
 
-        bruttolinesplit = [line for line in lines if "GESAMTBRUTTO:" in line][0].split(" ")
-        self.gesamtbrutto = bruttolinesplit[1] if len(bruttolinesplit) > 1 else "0,00"
+        index_start = self.line_index_with("Kosten- Kosten- Lohn") 
+        index_end = self.line_index_with("GESAMTBRUTTO")
+        lines_loehne = self._lines[index_start+3: index_end-1]
+
+        self.arbeitsmarktzulage = parse_float(self.line_with("Arbeitsmarktzulage").split(" ")[2]) if "Arbeitsmarktzulage" in "".join(lines_loehne) else 0
+        self.muenchenzulage = parse_float(self.line_with("Münchenzulage").split(" ")[2]) if "Münchenzulage" in "".join(lines_loehne) else 0
+
+        print("---")
+        print("\n".join(lines_loehne))
+        print("---")
+
+    def line_with(self, text: str):
+        return [line for line in self._lines if text in line][0]
+
+    def line_index_with(self, text: str):
+        return [index for index, line in enumerate(self._lines) if text in line][0]
 
 pages = []
 for pdf in pdfs:
+    print(f"Reading {pdf}...")
     text_pages = get_pages(pdf)
     for page in text_pages:
-        print(Page(page))
-        pages.append(Page(page))
-
-
+        page_obj = Page(page)
+        print(page_obj)
+        pages.append(page_obj)
